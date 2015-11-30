74888e76c7e72a42adcf717f12c2406ac2a22d52
Donny Davis <donny@cloudspin.me>
2015-11-30 14:35:19 -0500

    new updates

--- satellite6-1.sh
+++ satellite6-1.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
+echo ""
 subscription-manager repos --disable "*"
 subscription-manager repos --enable rhel-7-server-rpms --enable rhel-7-server-satellite-6.1-rpms --enable rhel-server-rhscl-7-rpms
 yum -y update 
 yum -y install katello
+firewall-cmd --direct --add-rule ipv4 filter OUTPUT 0 -o lo -p tcp -m tcp --dport 9200 -m owner --uid-owner foreman -j ACCEPT \
+&& firewall-cmd --direct --add-rule ipv6 filter OUTPUT 0 -o lo -p tcp -m tcp --dport 9200 -m owner --uid-owner foreman -j ACCEPT \
+&& firewall-cmd --direct --add-rule ipv4 filter OUTPUT 0 -o lo -p tcp -m tcp --dport 9200 -m owner --uid-owner root -j ACCEPT \
+&& firewall-cmd --direct --add-rule ipv6 filter OUTPUT 0 -o lo -p tcp -m tcp --dport 9200 -m owner --uid-owner root -j ACCEPT \
+&& firewall-cmd --direct --add-rule ipv4 filter OUTPUT 1 -o lo -p tcp -m tcp --dport 9200 -j DROP \
+&& firewall-cmd --direct --add-rule ipv6 filter OUTPUT 1 -o lo -p tcp -m tcp --dport 9200 -j DROP \
+&& firewall-cmd --permanent --direct --add-rule ipv4 filter OUTPUT 0 -o lo -p tcp -m tcp --dport 9200 -m owner --uid-owner foreman -j ACCEPT \
+&& firewall-cmd --permanent --direct --add-rule ipv6 filter OUTPUT 0 -o lo -p tcp -m tcp --dport 9200 -m owner --uid-owner foreman -j ACCEPT \
+&& firewall-cmd --permanent --direct --add-rule ipv4 filter OUTPUT 0 -o lo -p tcp -m tcp --dport 9200 -m owner --uid-owner root -j ACCEPT \
+&& firewall-cmd --permanent --direct --add-rule ipv6 filter OUTPUT 0 -o lo -p tcp -m tcp --dport 9200 -m owner --uid-owner root -j ACCEPT \
+&& firewall-cmd --permanent --direct --add-rule ipv4 filter OUTPUT 1 -o lo -p tcp -m tcp --dport 9200 -j DROP \
+&& firewall-cmd --permanent --direct --add-rule ipv6 filter OUTPUT 1 -o lo -p tcp -m tcp --dport 9200 -j DROP \
+&& firewall-cmd --permanent --add-port="53/udp" --add-port="53/tcp" \
+ --add-port="67/udp" --add-port="68/udp" \
+ --add-port="69/udp" --add-port="80/tcp" \
+ --add-port="443/tcp" --add-port="5647/tcp" \
+ --add-port="8140/tcp" \
+&& firewall-cmd --reload
+katello-installer \
+--foreman-admin-username dondavis \
+--foreman-admin-password changethispassword \
+--capsule-dns true \
+--capsule-dns-interface eth1 \
+--capsule-dns-zone fortnebula.com \
+--capsule-dns-forwarders 192.168.101.1 \
+--capsule-dns-reverse 101.168.192.in-addr.arpa \
+--capsule-dhcp true \
+--capsule-dhcp-interface eth1 \
+--capsule-dhcp-range "192.168.101.100 192.168.101.150" \
+--capsule-dhcp-gateway 192.168.101.1 \
+--capsule-dhcp-nameservers 192.168.101.2 \
+--capsule-tftp true \
+--capsule-tftp-servername $(hostname) \
+--capsule-puppet true \
+--capsule-puppetca true
