b9812207952a72ea5d8a6e20b491ded9d1a176f7
Donny Davis <donny@cloudspin.me>
2015-11-30 14:36:23 -0500

    Update satellite6-1.sh

--- satellite6-1.sh
+++ satellite6-1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo ""
 subscription-manager repos --disable "*"
 subscription-manager repos --enable rhel-7-server-rpms --enable rhel-7-server-satellite-6.1-rpms --enable rhel-server-rhscl-7-rpms
 yum -y update 
